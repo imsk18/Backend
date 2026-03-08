09158ed08daf7da121a567029f368b725b95f674
sk <kumarshatrudhan94263@gmail.com>
2026-03-08 22:18:09 +0530

    remove comments

--- day-1/app.js
+++ day-1/app.js
@@ -3,38 +3,28 @@
 
 // ✅ use of package
 
-const catMe = require("cat-me")
-catMe()
-console.log(catMe())
+// const catMe = require("cat-me")
+// catMe()
+// console.log(catMe())
 
 
 // ✅ how to crate server
 
-// const express = require("express")
-// const app = express() //server create kr chuke hai
+const express = require("express")
+const app = express() //server create kr chuke hai
 
 // app.get('/',(req, res)=>{
 //     res.send("hell0 world");
 // })
 
-// app.get('/home',(req,res )=>{
-//     res.send("welcome to home page");
-// })
+app.get('/home',(req,res )=>{
+    res.send("welcome to home page");
+})
 
-// app.get('/about',(req,res)=>{
-//     res.send("this is about page");
-// })
-
-
-// app.listen(8080) //(8080,7000,8000,5173,2000) //server ko start karta hai
+app.get('/about',(req,res)=>{
+    res.send("this is about page");
+})
 
 
+app.listen(8080) //(8080,7000,8000,5173,2000) //server ko start karta hai
 
-// const ex = require("express")
-// const app = ex()
-// app.get('/',(req ,res)=>{
-//     res.send('jay ho')
-// })
-
-
-//  app.listen(7000)
